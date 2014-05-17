901d172ee205dd985f8b8a757ec90e7552744d67
Patrik Gyllvin <patrikgyllvin@icloud.com>
2014-05-18 00:31:22 +0200

    Fixed some shit, added Main.cpp

--- src/Engine/Core/Window.h
+++ src/Engine/Core/Window.h
@@ -6,6 +6,8 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <string>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
---
+++ src/Game/Main.cpp
@@ -0,0 +1,11 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 18 maj 2014
+ *      Author: Patrik Gyllvin
+ */
+
+int main( int argc, char** argv )
+{
+	return 0;
+}
