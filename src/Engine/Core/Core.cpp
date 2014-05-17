8b34364b3d69676eddce456a2f34a55574251b40
Patrik Gyllvin <patrikgyllvin@icloud.com>
2014-05-17 23:41:36 +0200

    Added Core.cpp and Core.h

---
+++ src/Engine/Core/Core.cpp
@@ -0,0 +1,16 @@
+/*
+ * Core.cpp
+ *
+ *  Created on: 17 maj 2014
+ *      Author: Patrik Gyllvin
+ */
+
+#include "Core.h"
+
+namespace Engine {
+	Core::Core()
+	{}
+
+	Core::~Core()
+	{}
+}
---
+++ src/Engine/Core/Core.h
@@ -0,0 +1,19 @@
+/*
+ * Core.h
+ *
+ *  Created on: 17 maj 2014
+ *      Author: Patrik Gyllvin
+ */
+
+#ifndef CORE_H
+#define CORE_H
+
+namespace Engine {
+	class Core {
+	public:
+		Core();
+		virtual ~Core();
+	};
+}
+
+#endif
