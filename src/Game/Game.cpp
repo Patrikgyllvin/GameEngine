5ad1b36b242514daca014c1e8d1be31f7b18545c
Patrik Gyllvin <patrikgyllvin@icloud.com>
2014-05-18 12:18:27 +0200

    Removed unnecessary entity tests.

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -15,13 +15,6 @@ Game::~Game()
 
 void Game::init()
 {
-	Entity* e = entityManager->genEntity();
-	static_cast< TransformComponent* >( entityManager->addComponent( e, new TransformComponent() ) )->setPosition( 1.0, 1.0, 0.0 );
-	static_cast< MovementComponent* >( entityManager->addComponent( e, new MovementComponent() ) )->setVelocity( 0.0, 0.0, -0.2 );
-
-	Entity* a = entityManager->genEntity();
-	static_cast< TransformComponent* >( entityManager->addComponent( a, new TransformComponent() ) )->setPosition( 1.0, 1.0, 0.0 );
-	static_cast< MovementComponent* >( entityManager->addComponent( a, new MovementComponent() ) )->setVelocity( 0.0, 0.0, -0.2 );
 }
 
 void Game::tick()
