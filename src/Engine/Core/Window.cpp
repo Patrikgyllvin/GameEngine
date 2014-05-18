8ab20e9a9cb8c15eda127c554415c888cea56de3
Patrik Gyllvin <patrikgyllvin@icloud.com>
2014-05-18 17:52:44 +0200

    Re-added Core profile on OS X.

--- src/Engine/Core/Window.cpp
+++ src/Engine/Core/Window.cpp
@@ -19,10 +19,10 @@ namespace Engine
 	{
 		glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
 		glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+		glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+
 #ifdef __APPLE__
 		glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, 1 );
-#else
-		glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 #endif
 		
 		if( resizable )
