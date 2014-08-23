a71c50f3d813eb99762c64bce90441c1a48008b4
Patrik Gyllvin <patrikgyllvin@icloud.com>
2014-08-23 10:43:39 +0200

    Math.

---
+++ Source/include/Engine/Math/Vec3.h
@@ -0,0 +1,64 @@
+#ifndef Vec3_H
+#define Vec3_H
+
+#include <string>
+
+#include <cmath>
+
+namespace Engine::Math
+{
+	class vec3
+	{
+	public:
+		Vec3();
+		Vec3( float x, float y, float z );
+
+		~Vec3();
+
+		std::string toString();
+
+		float dot( const Vec3& otherVec );
+		Vec3 cross( const Vec3& otherVec );
+
+		float length();
+		Vec3 normalized();
+
+		void setX( float x );
+		void setY( float y );
+		void setZ( float z );
+
+		float getX() const;
+		float getY() const;
+		float getZ() const;
+
+		Vec3 operator+( const Vec3& otherVec );
+		Vec3 operator+( float f );
+
+		Vec3 operator-( const Vec3& otherVec );
+		Vec3 operator-( float f );
+
+		//Vec3 operator*( const Vec3& otherVec );
+		Vec3 operator*( float factor );
+
+		//Vec3 operator/( const Vec3& otherVec );
+		Vec3 operator/( float denominator );
+
+
+		Vec3& operator+=( const Vec3& otherVec );
+		Vec3& operator+=( float f );
+
+		Vec3& operator-=( const Vec3& otherVec );
+		Vec3& operator-=( float f );
+
+		Vec3& operator*=( const Vec3& otherVec );
+		Vec3& operator*=( float factor );
+
+		Vec3& operator/=( const Vec3& otherVec );
+		Vec3& operator/=( float denominator );
+
+	private:
+		float x, y, z;
+	};
+}
+
+#endif
---
+++ Source/src/Engine/Math/Vec3.cpp
@@ -0,0 +1,111 @@
+#include "Engine/Math/Vec3.h"
+
+namespace Engine::Math
+{
+	Vec3::Vec3()
+	:
+	x( 0 ),
+	y( 0 ),
+	z( 0 )
+	{}
+
+	Vec3::Vec3( float x, float y, float z )
+	:
+	x( x ),
+	y( y ),
+	z( z )
+	{}
+
+	Vec3::~Vec3(){}
+
+	std::string Vec3::toString( )
+	{
+		return std::string(x + " " + y + " " + z);
+	}
+
+	float Vec3::dot( const Vec3& otherVec )
+	{
+		return x * otherVec.getX() + y * otherVec.getY() + z * otherVec.getZ();
+	}
+
+	Vec3 Vec3::cross( const Vec3& otherVec )
+	{
+		float f0 = y * otherVec.getZ( ) - z * otherVec.getY( );
+		float f1 = z * otherVec.getX( ) - x * otherVec.getZ( );
+		float f2 = x * otherVec.getY( ) - y * otherVec.getX( );
+
+		return Vec3( f0, f1, f2 );
+	}
+
+	float Vec3::length()
+	{
+		return (float)sqrt( dot( *this ) );
+	}
+
+	Vec3 Vec3::normalized()
+	{
+		float len = length();
+
+		if( len != 0 )
+		{
+			return Vec3( this->x / len, this->y / len, this->z / len )
+		}
+
+		return *this;
+	}
+
+	void Vec3::setX( float x )
+	{
+		this->x = x;
+	}
+
+	void Vec3::setY( float y )
+	{
+		this->y = y;
+	}
+
+	void Vec3::setZ( float z )
+	{
+		this->z = z;
+	}
+
+	float Vec3::getX() const
+	{
+		return this->x;
+	}
+	
+	float Vec3::getY() const
+	{
+		return this->y;
+	}
+
+	float Vec3::getZ() const
+	{
+		return this->z;direction vector
+	}
+
+	Vec3 Vec3::operator+( const Vec3& otherVec )
+	{direction vector
+		return Vec3( x + otherVec.getX(), y + otherVec.getY(), z + otherVec.getZ() );
+	}
+
+	Vec3 Vec3::operator-( const Vec3& otherVec )
+	{
+		return Vec3( x - otherVec.getX(), y - otherVec.getY(), z - otherVec.getZ() );
+	}
+
+	Vec3 Vec3::operator*( float factor )
+	{
+		return Vec3( x * factor, y * factor, z * factor );
+	}
+
+	Vec3 Vec3::operator/( float denominator )
+	{
+		return Vec3( x / denominator, y / denominator, z / denominator );
+	}
+
+	Vec3& Vec3::operator+=( const Vec3& otherVec ){}
+	Vec3& Vec3::operator-=( const Vec3& otherVec ){}
+	Vec3& Vec3::operator*=( float factor ){}
+	Vec3& Vec3::operator/=( float denominator ){}
+}
